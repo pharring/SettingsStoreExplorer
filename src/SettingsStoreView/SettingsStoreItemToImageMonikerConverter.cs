9093495332ca3e6cbf97e533e81d380da5c80830
Paul Harrington <pharring@users.noreply.github.com>
2019-04-29 12:42:37 -0700

    Icons (#17)
    * Add icons to tree nodes
    
    * Add icons to property view

---
+++ src/SettingsStoreView/SettingsStoreItemToImageMonikerConverter.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Paul Harrington.  All Rights Reserved.  Licensed under the MIT License.  See LICENSE in the project root for license information.
+
+using System.Globalization;
+using Microsoft.VisualStudio.Imaging.Interop;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell.Interop;
+using VsKnownMonikers = Microsoft.VisualStudio.Imaging.KnownMonikers;
+
+namespace SettingsStoreView
+{
+    internal class SettingsStoreItemToImageMonikerConverter : ValueConverter<SettingsStoreItem, ImageMoniker>
+    {
+        protected override ImageMoniker Convert(SettingsStoreItem value, object parameter, CultureInfo culture)
+        {
+            switch(value)
+            {
+                case RootSettingsStore rootItem:
+                    switch (rootItem.EnclosingScope)
+                    {
+                        case __VsEnclosingScopes.EnclosingScopes_UserSettings:
+                            return VsKnownMonikers.User;
+
+                        default:
+                            return KnownMonikers.SettingsStoreViewToolWindowCommand;
+                    }
+
+                case SettingsStoreSubCollection _:
+                    return VsKnownMonikers.FolderClosed;
+
+                default:
+                    return base.Convert(value, parameter, culture);
+            }
+        }
+    }
+}
---
+++ src/SettingsStoreView/SettingsStoreTypeToImageMonikerConverter.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Paul Harrington.  All Rights Reserved.  Licensed under the MIT License.  See LICENSE in the project root for license information.
+
+using System.Globalization;
+using Microsoft.VisualStudio.Imaging.Interop;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell.Interop;
+using VsKnownMonikers = Microsoft.VisualStudio.Imaging.KnownMonikers;
+
+namespace SettingsStoreView
+{
+    internal class SettingsStoreTypeToImageMonikerConverter : ValueConverter<__VsSettingsType, ImageMoniker>
+    {
+        protected override ImageMoniker Convert(__VsSettingsType value, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case __VsSettingsType.SettingsType_Int:
+                case __VsSettingsType.SettingsType_Int64:
+                    return VsKnownMonikers.Numeric;
+
+                case __VsSettingsType.SettingsType_String:
+                    return VsKnownMonikers.String;
+
+                case __VsSettingsType.SettingsType_Binary:
+                    return VsKnownMonikers.Binary;
+
+                default:
+                    return base.Convert(value, parameter, culture);
+            }
+        }
+    }
+}
