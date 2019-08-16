2e4babf0bd187b04e9e13cade7293f2a10155f5f
Paul Harrington <pharring@microsoft.com>
2019-08-15 20:16:43 -0700

    Set assembly and VSIX version from a single setting

--- src/SettingsStoreExplorer/Properties/AssemblyInfo.cs
+++ src/SettingsStoreExplorer/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,10 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Settings Store Explorer")]
 [assembly: AssemblyDescription("A Visual Studio Extension with a tool window for viewing and editing the Visual Studio settings store.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Paul Harrington")]
 [assembly: AssemblyProduct("SettingsStoreExplorer")]
-[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,16 +14,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
